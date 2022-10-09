d80cb9266c315d8ac04258e46857ffa0e59897b7
faisal-shohag <faisalshohagprog@gmail.com>
2022-10-09 11:27:19 +0600

    final

--- scripts/auth.js
+++ scripts/auth.js
@@ -319,4 +319,33 @@ firebase
     // ...
   });
 
+  firebase.auth().signInWithRedirect(provider);
+
+  firebase.auth()
+  .getRedirectResult()
+  .then((result) => {
+    if (result.credential) {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+    console.log(result);
+    console.log(user);
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+
 }
