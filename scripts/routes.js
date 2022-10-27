991dc4a094cd86740a0076dc1197568baaee0f1c
faisal-shohag <faisalshohagprog@gmail.com>
2022-10-27 09:32:02 +0600

    v1

--- scripts/routes.js
+++ scripts/routes.js
@@ -3392,14 +3392,14 @@ const myWidget = cloudinary.createUploadWidget(
     cropping: true, //add a cropping step
     showAdvancedOptions: true,  //add advanced options (public_id and tag)
     sources: [ "local"], // restrict the upload sources to URL and local files
-    // multiple: false,  //restrict upload to a single file
+    multiple: true,  //restrict upload to a single file
     // folder: "user_images", //upload files to the specified folder
     // tags: ["users", "profile"], //add the given tags to the uploaded files
     // context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
-    // clientAllowedFormats: ["images"], //restrict uploading to image files only
+    clientAllowedFormats: ["images", "jpg", "png"], //restrict uploading to image files only
     maxImageFileSize: 2000000,  //restrict file size to less than 2MB
     // maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
-    theme: "purple", //change to a purple theme
+    theme: "indigo", //change to a purple theme
   },
   (error, result) => {
     if (!error && result && result.event === "success") {
